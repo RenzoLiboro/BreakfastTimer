bbaa2b07bd4827c3c41527e505c56105096811a9
RenzoLiboro <renzoliboro@gmail.com>
2025-02-13 13:15:36 -0800

    Update 1 app.js

--- app.js
+++ app.js
@@ -6,89 +6,66 @@ let baconCountdown;
 let eggsCountdown;
 let pancakesCountdown;
 
-let baconTime = 180;  // 3 minutes for bacon
-let eggsTime = 120;  // 2 minutes for 1 scrambled egg
-let pancakesTime = 150;  // 2.5 minutes for pancakes
+let baconTime = 180;  
+let eggsTime = 120;  
+let pancakesTime = 150;  
 
 let baconPaused = false;
 let eggsPaused = false;
 let pancakesPaused = false;
 
-let sizzlingSound = new Audio('BreakfastTimer/Audio/Wait.mp3'); // Sizzling sound (looping)
-let beepSound = new Audio('BreakfastTimer/Audio/Bell.mp3'); // Beep sound
+let canPressBacon = true;
+let canPressEggs = true;
+let canPressPancakes = true;
 
-document.getElementById('startBacon').onclick = function() {
-  startBacon();
-};
+let sizzlingSound = new Audio('BreakfastTimer/Audio/Wait.mp3'); 
+let beepSound = new Audio('BreakfastTimer/Audio/Bell.mp3'); 
 
-document.getElementById('pauseBacon').onclick = function() {
-  pauseBacon();
-};
+document.getElementById('startBacon').onclick = function() { startBacon(); };
+document.getElementById('pauseBacon').onclick = function() { pauseBacon(); };
+document.getElementById('resetBacon').onclick = function() { resetBacon(); };
 
-document.getElementById('resetBacon').onclick = function() {
-  resetBacon();
-};
+document.getElementById('startEggs').onclick = function() { startEggs(); };
+document.getElementById('pauseEggs').onclick = function() { pauseEggs(); };
+document.getElementById('resetEggs').onclick = function() { resetEggs(); };
 
-document.getElementById('startEggs').onclick = function() {
-  startEggs();
-};
-
-document.getElementById('pauseEggs').onclick = function() {
-  pauseEggs();
-};
-
-document.getElementById('resetEggs').onclick = function() {
-  resetEggs();
-};
-
-document.getElementById('startPancakes').onclick = function() {
-  startPancakes();
-};
-
-document.getElementById('pausePancakes').onclick = function() {
-  pausePancakes();
-};
-
-document.getElementById('resetPancakes').onclick = function() {
-  resetPancakes();
-};
+document.getElementById('startPancakes').onclick = function() { startPancakes(); };
+document.getElementById('pausePancakes').onclick = function() { pausePancakes(); };
+document.getElementById('resetPancakes').onclick = function() { resetPancakes(); };
 
 document.getElementById('eggType').onchange = function() {
   let eggCountDropdown = document.getElementById('eggCount');
-  if (this.value === 'sunnySideUp') {
-    eggCountDropdown.style.display = 'none';
-  } else {
-    eggCountDropdown.style.display = 'block';
-  }
+  eggCountDropdown.style.display = this.value === 'sunnySideUp' ? 'none' : 'block';
 };
 
 function startBacon() {
-  baconPaused = false;
-  baconTimer.innerText = formatTime(baconTime);
-  sizzlingSound.loop = true;
-  sizzlingSound.play();
-  
-  baconCountdown = setInterval(function() {
-    if (!baconPaused) {
-      if (baconTime > 0) {
-        baconTime--;
-        baconTimer.innerText = formatTime(baconTime);
-      } else {
-        clearInterval(baconCountdown);
-        sizzlingSound.pause();
-        sizzlingSound.currentTime = 0;
-        alert('Bacon done!');
-        beepBeep();
+  if (canPressBacon) {
+    canPressBacon = false;
+    baconPaused = false;
+    baconTimer.innerText = formatTime(baconTime);
+    sizzlingSound.loop = true;
+    sizzlingSound.play();
+    
+    baconCountdown = setInterval(function() {
+      if (!baconPaused) {
+        if (baconTime > 0) {
+          baconTime--;
+          baconTimer.innerText = formatTime(baconTime);
+        } else {
+          clearInterval(baconCountdown);
+          sizzlingSound.pause();
+          sizzlingSound.currentTime = 0;
+          alert('Bacon done!');
+          beepBeep();
+        }
       }
-    }
-  }, 1000);
+    }, 1000);
+  }
 }
 
-function pauseBacon() {
-  baconPaused = true;
-}
-
+function pauseBacon() { canPressBacon = true; baconPaused = true; }
 function resetBacon() {
+  canPressBacon = true;
   baconTime = 180;
   baconTimer.innerText = '';
   clearInterval(baconCountdown);
@@ -97,49 +74,38 @@ function resetBacon() {
 }
 
 function startEggs() {
-  const eggCount = parseInt(document.getElementById('eggCount').value);
-  const eggType = document.getElementById('eggType').value;
-  
-  if (eggType === 'scrambled') {
-    if (eggCount === 1) {
-      eggsTime = 120;
-    } else if (eggCount === 2) {
-      eggsTime = 150;
-    }else if (eggCount === 3) {
-      eggsTime = 180;
-    } else {
-      eggsTime = 270; // 4+ eggs = 4 minutes 30 seconds
-    }
-  } else if (eggType === 'sunnySideUp') {
-    eggsTime = 120;
-  }
+  if (canPressEggs) {
+    canPressEggs = false;
+    const eggCount = parseInt(document.getElementById('eggCount').value);
+    const eggType = document.getElementById('eggType').value;
+    
+    eggsTime = eggType === 'scrambled' ? (eggCount === 1 ? 120 : eggCount === 2 ? 150 : eggCount === 3 ? 180 : 270) : 120;
+    
+    eggsPaused = false;
+    eggsTimer.innerText = formatTime(eggsTime);
+    sizzlingSound.loop = true;
+    sizzlingSound.play();
 
-  eggsPaused = false;
-  eggsTimer.innerText = formatTime(eggsTime);
-  sizzlingSound.loop = true;
-  sizzlingSound.play();
-
-  eggsCountdown = setInterval(function() {
-    if (!eggsPaused) {
-      if (eggsTime > 0) {
-        eggsTime--;
-        eggsTimer.innerText = formatTime(eggsTime);
-      } else {
-        clearInterval(eggsCountdown);
-        sizzlingSound.pause();
-        sizzlingSound.currentTime = 0;
-        alert('Eggs done!');
-        beepBeep();
+    eggsCountdown = setInterval(function() {
+      if (!eggsPaused) {
+        if (eggsTime > 0) {
+          eggsTime--;
+          eggsTimer.innerText = formatTime(eggsTime);
+        } else {
+          clearInterval(eggsCountdown);
+          sizzlingSound.pause();
+          sizzlingSound.currentTime = 0;
+          alert('Eggs done!');
+          beepBeep();
+        }
       }
-    }
-  }, 1000);
+    }, 1000);
+  }
 }
 
-function pauseEggs() {
-  eggsPaused = true;
-}
-
+function pauseEggs() { canPressEggs = true; eggsPaused = true; }
 function resetEggs() {
+  canPressEggs = true;
   eggsTime = 120;
   eggsTimer.innerText = '';
   clearInterval(eggsCountdown);
@@ -148,32 +114,33 @@ function resetEggs() {
 }
 
 function startPancakes() {
-  pancakesPaused = false;
-  pancakesTimer.innerText = formatTime(pancakesTime);
-  sizzlingSound.loop = true;
-  sizzlingSound.play();
+  if (canPressPancakes) {
+    canPressPancakes = false;
+    pancakesPaused = false;
+    pancakesTimer.innerText = formatTime(pancakesTime);
+    sizzlingSound.loop = true;
+    sizzlingSound.play();
 
-  pancakesCountdown = setInterval(function() {
-    if (!pancakesPaused) {
-      if (pancakesTime > 0) {
-        pancakesTime--;
-        pancakesTimer.innerText = formatTime(pancakesTime);
-      } else {
-        clearInterval(pancakesCountdown);
-        sizzlingSound.pause();
-        sizzlingSound.currentTime = 0;
-        alert('Pancakes done!');
-        beepBeep();
+    pancakesCountdown = setInterval(function() {
+      if (!pancakesPaused) {
+        if (pancakesTime > 0) {
+          pancakesTime--;
+          pancakesTimer.innerText = formatTime(pancakesTime);
+        } else {
+          clearInterval(pancakesCountdown);
+          sizzlingSound.pause();
+          sizzlingSound.currentTime = 0;
+          alert('Pancakes done!');
+          beepBeep();
+        }
       }
-    }
-  }, 1000);
+    }, 1000);
+  }
 }
 
-function pausePancakes() {
-  pancakesPaused = true;
-}
-
+function pausePancakes() { canPressPancakes = true; pancakesPaused = true; }
 function resetPancakes() {
+  canPressPancakes = true;
   pancakesTime = 150;
   pancakesTimer.innerText = '';
   clearInterval(pancakesCountdown);
@@ -189,8 +156,6 @@ function formatTime(time) {
 
 function beepBeep() {
   for (let i = 0; i < 3; i++) {
-    setTimeout(() => {
-      beepSound.play();
-    }, i * 1000);
+    setTimeout(() => { beepSound.play(); }, i * 1000);
   }
 }
